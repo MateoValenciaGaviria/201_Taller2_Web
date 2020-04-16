77f981c6ec8ce37b937c532e5646f58ded599cb3
Mateo <mateo.valencia.g@gmail.com>
2020-04-16 12:41:12 -0500

    handlebars files

---
+++ server.js
@@ -0,0 +1,52 @@
+//Importar express
+const express = require('express');
+
+//Importar path
+const path = require('path');
+
+//Importar express-handlebars
+const exphbs = require('express-handlebars');
+
+//Instanciar servidor de express
+const app = express();
+
+//Registrar motor de render para handle-bars
+app.engine('handlebars', exphbs());
+
+//Use el motor de render handle-bars
+app.set('view engine', 'handlebars');
+
+app.use(express.static('public'));
+
+//Configurar ruta inicial
+app.get('/', function (req, res){
+    console.log('hola en la consola');
+    //Responder con un archivo
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+//Ruta para la lista de productos con handlebars
+app.get('/store', function (req, res){
+    //Renderizar vista
+    res.render('store');
+});
+
+//Ruta para especificaciones de un producto con handlebars
+app.get('/product', function (req, res){
+    //Renderizar vista
+    res.render('product');
+});
+
+//Ruta para el checkout con handlebars
+app.get('/checkout', function (req, res){
+    //Renderizar vista
+    res.render('checkout');
+});
+ 
+//Iniciar servidor en puerto 3000
+app.listen(3000, function (){
+    console.log('servidor iniciado en puerto 3000');
+});
+
+//npm - node package manager
+//npx - node package executer nodemon para actualizar 
