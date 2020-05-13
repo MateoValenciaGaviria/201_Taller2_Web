cf03e561c552ad65a7769982e0b1311fb164c19e
Mateo <mateo.valencia.g@gmail.com>
2020-05-13 01:42:24 -0500

    check out working

--- public/scripts/cart.js
+++ public/scripts/cart.js
@@ -3,14 +3,41 @@ var buttonProductSpecs = document.querySelectorAll('.productspecs__button');
 var cartNumber = document.querySelector('.mainheader__shopcartnumber');
 var cartList = [];
 
-if(localStorage.getItem('cartList')){
+if (localStorage.getItem('cartList')) {
     cartList = JSON.parse(localStorage.getItem('cartList'));
 }
 
 cartNumber.innerText = cartList.length;
 
-buttons.forEach(function(elem){
-    elem.addEventListener('click', function(){
+function renderCart() {
+    var cartContainer = document.querySelector('.checkout__container');
+    cartList.forEach(function (obj) {
+        var newItem = document.createElement('div');
+        newItem.innerHTML = `
+       <div class="checkout__product">
+       <hr class="checkout__line">
+       <div class="checkout__infocontainer">
+           <div class="checkout__imgname">
+               <div class="checkout__productimg">
+                   <img class="checkout__image" src="`+obj.img+`" alt="`+obj.name+`">
+               </div>
+               <div class="checkout__productname">
+                    `+obj.name+`
+               </div>
+           </div>
+           <div class="checkout__productprice">
+               $ `+obj.price+`.00
+           </div>
+       </div>
+       </div>
+       `;
+        cartContainer.appendChild(newItem);
+    });
+}
+renderCart();
+
+buttons.forEach(function (elem) {
+    elem.addEventListener('click', function () {
         /* var name = elem.parentElement.parentElement.querySelector('.product__name').innerText; */
         var id = elem.getAttribute('data-id');
         var name = elem.getAttribute('data-name');
@@ -21,15 +48,15 @@ buttons.forEach(function(elem){
             id: id,
             name: name,
             img: img,
-            price: price 
+            price: price
         });
         cartNumber.innerText = cartList.length;
         localStorage.setItem('cartList', JSON.stringify(cartList));
     });
 });
 
-buttonProductSpecs.forEach(function(elem){
-    elem.addEventListener('click', function(){
+buttonProductSpecs.forEach(function (elem) {
+    elem.addEventListener('click', function () {
         /* var name = elem.parentElement.parentElement.querySelector('.product__name').innerText; */
         var id = elem.getAttribute('data-id');
         var name = elem.getAttribute('data-name');
@@ -40,7 +67,7 @@ buttonProductSpecs.forEach(function(elem){
             id: id,
             name: name,
             img: img,
-            price: price 
+            price: price
         });
         cartNumber.innerText = cartList.length;
         localStorage.setItem('cartList', JSON.stringify(cartList));
